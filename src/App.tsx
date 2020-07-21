4ea9b1e3b85644c3bf362e791c7110a4e4774d62
Arkadiusz <arkadiusz@ostrowski.co>
2020-07-21 23:44:35 +0100

    update README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
     </div>
   );
 }
-
-export default App;
