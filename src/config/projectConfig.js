8412f206e4da141a304d5f8558443d15a7610792
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-23 21:55:49 +0100

    add project colours and fonts config file

---
+++ src/config/projectConfig.js
@@ -0,0 +1,41 @@
+export const colors = {
+  primary: {
+    scissors: {
+      gradientOne: "hsl(39, 89%, 49%)",
+      gradientTwo: "hsl(40, 84%, 53%)",
+    },
+    paper: {
+      gradientOne: "hsl(230, 89%, 62%)",
+      gradientTwo: "hsl(230, 89%, 65%)",
+    },
+    rock: {
+      gradientOne: "hsl(349, 71%, 52%)",
+      gradientTwo: "hsl(349, 70%, 56%)",
+    },
+    lizard: {
+      gradientOne: "hsl(261, 73%, 60%)",
+      gradientTwo: "hsl(261, 72%, 63%)",
+    },
+    spock: {
+      gradientOne: "hsl(189, 59%, 53%)",
+      gradientTwo: "hsl(189, 58%, 57%)",
+    },
+  },
+  neutral: {
+    darkText: "hsl(229, 25%, 31%)",
+    scoreText: "hsl(229, 64%, 46%)",
+    headerOutline: "hsl(217, 16%, 45%)",
+  },
+  background: {
+    gradientOne: "hsl(214, 47%, 23%)",
+    gradientTwo: "hsl(237, 49%, 15%)",
+  },
+};
+
+export const fonts = {
+  family: "Barlow Semi Condensed",
+  weights: {
+    bold: 600,
+    bolder: 700,
+  },
+};
